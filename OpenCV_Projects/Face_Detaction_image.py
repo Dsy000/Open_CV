efa42901e84dd1e602bc5f34687788577eb1e9bb
Google-D <52644394+Google-D@users.noreply.github.com>
2020-06-06 12:28:52 +0530

    some Chang

--- OpenCV_Projects/Face_Detaction_image.py
+++ OpenCV_Projects/Face_Detaction_image.py
@@ -1,4 +1,5 @@
 #This Facedetaction example 
+#-Created_by_Deepak_yadav
 #
 import cv2
 
@@ -8,10 +9,12 @@ img=cv2.imread("data/space.jpg") #image file location
 
 imgGray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) #converting Gray scal image(Blaken-wite)
 face=faceCascade.detectMultiScale(imgGray,1.1,4)
-
+i=0
 print(face)
 for(x,y,w,h) in face:
+    print(i,"=","x",x,"y",y,"w",w,"h",h)
     cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2) #create Blue ractangle on around face 
-
+    i=i+1
+print("I am Find ",i,"Face in this image")
 cv2.imshow("Output",img) #show Face output
 cv2.waitKey(0)  
